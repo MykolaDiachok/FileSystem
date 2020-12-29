6233543ee89a55c90ec8e8685970211ab29d8743
Mykola Diachok <ndyachok@gmail.com>
2020-12-29 12:33:58 +0200

    Add project files.

---
+++ FileSystem/Core/Base/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FileSystem.Core.Base
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool Deleted { get; set; }
+    }
+}
---
+++ FileSystem/Core/Base/File.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FileSystem.Core.Base
+{
+    public class File
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Category Category { get; set; }
+        public Directory ParentDirectory { get; set; }
+        public bool Deleted { get; set; }
+    }
+}
